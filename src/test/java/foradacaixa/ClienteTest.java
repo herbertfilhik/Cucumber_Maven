2a0698d2a6153eb920d1088950dfee879f21db2d
default <default@Lindao.local>
2017-06-14 16:53:45 -0300

    first commit

---
+++ src/test/java/foradacaixa/ClienteTest.java
@@ -0,0 +1,14 @@
+package foradacaixa;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java",
+				 glue = {""}, monochrome = true, dryRun = false)
+
+public class ClienteTest {
+
+}
